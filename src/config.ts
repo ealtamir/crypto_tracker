0f5cfd72908746122f03852cfb535d7deadebf29
Enzo Altamiranda <enzo.alt@gmail.com>
2018-05-20 13:35:20 -0300

    Added environment variables to ease deployment in production.

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,21 @@
 import * as fs from 'fs'
 import * as _ from 'lodash'
+import * as process from 'process'
 
-let standard = JSON.parse(fs.readFileSync('config.json', { encoding: 'utf8' }))
-let secrets = JSON.parse(fs.readFileSync('secrets.json', { encoding: 'utf8' }))
+
+let CRYPTO_TRACKER_CONFIG_FILE_PATH = process.env.CRYPTO_TRACKER_CONFIG_FILE_PATH
+let CRYPTO_TRACKER_SECRET_FILE_PATH = process.env.CRYPTO_TRACKER_SECRET_FILE_PATH
+
+if (!CRYPTO_TRACKER_CONFIG_FILE_PATH) {
+    throw new Error("CONFIG_FILE_PATH env variable should be set to the config file location")
+}
+
+if (!CRYPTO_TRACKER_SECRET_FILE_PATH)  {
+    throw new Error("SECRET_FILE_PATH env variable should be set to the config file location")
+}
+
+let standard = JSON.parse(fs.readFileSync(CRYPTO_TRACKER_CONFIG_FILE_PATH, { encoding: 'utf8' }))
+let secrets = JSON.parse(fs.readFileSync(CRYPTO_TRACKER_SECRET_FILE_PATH, { encoding: 'utf8' }))
 
 const config = _.extend({secrets}, standard)
 
